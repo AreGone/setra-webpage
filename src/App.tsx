987718e807139167933dedcfec2a736a22b21a11
Melika Arab <melikaly19@gmail.com>
2025-09-27 11:51:12 +0330

    creating a sample layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import { Box } from "@chakra-ui/react";
 
 function App() {
-  return <Box>Helo world</Box>;
+  return (
+    <Box>
+      <Box backgroundColor={"pink"}>Navbar</Box>
+      <Box backgroundColor={"gold"}>Hero Section</Box>
+      <Box backgroundColor={"purple"}>Stats Section</Box>
+      <Box backgroundColor={"red"}>AI Feature Section</Box>
+      <Box backgroundColor={"blueviolet"}>How it works Section</Box>
+      <Box backgroundColor={"lavender"}>Benifit Section</Box>
+      <Box backgroundColor={"lightcoral"}>CTA Section</Box>
+      <Box backgroundColor={"gray"}>footer</Box>
+    </Box>
+  );
 }
 
 export default App;
